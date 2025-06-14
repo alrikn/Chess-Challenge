7695a2a608a6acd2ce4d49e221e5349a7faa930b
Alrik Neihouser <alrik.neihouser@epitech.eu>
2025-06-14 20:45:09 +0200

    feat: evilbot is now reference for real

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -10,98 +10,98 @@ namespace ChessChallenge.Example
     public class EvilBot : IChessBot
     {
         bool MoveIsCheckmate(Board board, Move move)
-    {
-        board.MakeMove(move);
-        bool isMate = board.IsInCheckmate();
-        board.UndoMove(move);
-        return isMate;
-    }
-    Move return_nice_pos(Board board, Move[] all_moves, ref int score)
-    {
-        Random rng = new();
-        Move move_result = all_moves[rng.Next(all_moves.Length)];
-
-        foreach (Move move in all_moves)
         {
-            if (MoveIsCheckmate(board, move))
-            {
-                move_result = move;
-                break;
-            }
-            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-            if (capturedPieceValue > score)
-            {
-                move_result = move;
-                score = capturedPieceValue;
-            }
+            board.MakeMove(move);
+            bool isMate = board.IsInCheckmate();
+            board.UndoMove(move);
+            return isMate;
         }
-        return move_result;
-    }
-
-    /*
-    ** essentially, we take the best move we have currently found,
-    ** and we check that the next opponent move will not absolutely
-    ** annihilate us (as in we are shooting ourself in foot)
-    ** returns true if actually we have bad move, and false if its ok
-    */
-    bool foot_is_shot(Board board, int score, Move best_move)
-    {
-        board.MakeMove(best_move);
-        Move[] all_moves = board.GetLegalMoves();
-        bool bad = false;
-        foreach (Move move in all_moves)
+        Move return_nice_pos(Board board, Move[] all_moves, ref int score)
         {
-            if (MoveIsCheckmate(board, move))
+            Random rng = new();
+            Move move_result = all_moves[rng.Next(all_moves.Length)];
+    
+            foreach (Move move in all_moves)
             {
-                bad = true;
-                break;
+                if (MoveIsCheckmate(board, move))
+                {
+                    move_result = move;
+                    break;
+                }
+                Piece capturedPiece = board.GetPiece(move.TargetSquare);
+                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+                if (capturedPieceValue > score)
+                {
+                    move_result = move;
+                    score = capturedPieceValue;
+                }
             }
-            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-            if (score - capturedPieceValue < 0)
-            {
-                bad = true;
-                break;
-            }
-        }
-        board.UndoMove(best_move);
-        //if bad is true we have to add bad_move to a list of illegal moves
-        return bad;
-    }
-
-    // Piece values: null, pawn, knight, bishop, rook, queen, king
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-    public Move Think(Board board, Timer timer)
-    {
-        Move[] all_moves = board.GetLegalMoves();
-        List<Move> bad_moves = new List<Move>();
-        Random rng = new();
-        Move move_result = all_moves[rng.Next(all_moves.Length)];
-        int score = 0;
-
-        start:
-        //score = 0;
-        /*if i put score = 0 here, my bot suddenly becomes much worse.
-        why?*/
-        // remove bad moves
-        Move[] legal_moves = all_moves.Where(m => !bad_moves.Contains(m)).ToArray();
-
-        // If all moves are bad, return a default one
-        if (legal_moves.Length == 0)
-        {
             return move_result;
         }
-
-        move_result = return_nice_pos(board, legal_moves, ref score);
-
-        if (foot_is_shot(board, score, move_result))
+    
+        /*
+        ** essentially, we take the best move we have currently found,
+        ** and we check that the next opponent move will not absolutely
+        ** annihilate us (as in we are shooting ourself in foot)
+        ** returns true if actually we have bad move, and false if its ok
+        */
+        bool foot_is_shot(Board board, int score, Move best_move)
         {
-            bad_moves.Add(move_result);
-            goto start;
+            board.MakeMove(best_move);
+            Move[] all_moves = board.GetLegalMoves();
+            bool bad = false;
+            foreach (Move move in all_moves)
+            {
+                if (MoveIsCheckmate(board, move))
+                {
+                    bad = true;
+                    break;
+                }
+                Piece capturedPiece = board.GetPiece(move.TargetSquare);
+                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+                if (score - capturedPieceValue < 0)
+                {
+                    bad = true;
+                    break;
+                }
+            }
+            board.UndoMove(best_move);
+            //if bad is true we have to add bad_move to a list of illegal moves
+            return bad;
         }
-        //Console.WriteLine("MyBot plays: " + move_result);
-        return move_result;
-    }
+    
+        // Piece values: null, pawn, knight, bishop, rook, queen, king
+        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        public Move Think(Board board, Timer timer)
+        {
+            Move[] all_moves = board.GetLegalMoves();
+            List<Move> bad_moves = new List<Move>();
+            Random rng = new();
+            Move move_result = all_moves[rng.Next(all_moves.Length)];
+            int score = 0;
+    
+            start:
+            //score = 0;
+            /*if i put score = 0 here, my bot suddenly becomes much worse.
+            why?*/
+            // remove bad moves
+            Move[] legal_moves = all_moves.Where(m => !bad_moves.Contains(m)).ToArray();
+    
+            // If all moves are bad, return a default one
+            if (legal_moves.Length == 0)
+            {
+                return move_result;
+            }
+    
+            move_result = return_nice_pos(board, legal_moves, ref score);
+    
+            if (foot_is_shot(board, score, move_result))
+            {
+                bad_moves.Add(move_result);
+                goto start;
+            }
+            //Console.WriteLine("MyBot plays: " + move_result);
+            return move_result;
+        }
     }
 }
